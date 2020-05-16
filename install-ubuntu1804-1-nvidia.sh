5739177b687a7bb36a039bb92eb04ab47451e3e6
Kelvin Kang <kelvinkang@cmu.edu>
2020-05-16 21:54:41 +0800

    update cuda and autoware

--- install-ubuntu1804-1-nvidia.sh
+++ install-ubuntu1804-1-nvidia.sh
@@ -1,27 +1,45 @@
-#!/bin/bash
+# #!/bin/bash
 # https://gist.github.com/Mahedi-61/2a2f1579d4271717d421065168ce6a73
 echo "#######################install nvidia########################\n"
-sudo apt update
-sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
-sudo apt install linux-headers-$(uname -r) build-essential
-cd utils
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo apt-get purge nvidia*
+sudo apt remove nvidia-*
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt-get autoremove && sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*
 
-sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+
+### to verify your gpu is cuda enable check
+lspci | grep -i nvidia
+
+### gcc compiler is required for development using the cuda toolkit. to verify the version of gcc install enter
+gcc --version
+
+# system update
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get upgrade
 
-echo 'export PATH=/usr/local/cuda-10.2/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+
+# install other import packages
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+
+# first get the PPA repository driver
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+
+ # installing CUDA-10.1
+sudo apt-get -o Dpkg::Options::="--force-overwrite" install cuda-10-0 cuda-drivers
+
+echo 'export PATH=/usr/local/cuda-10.0/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
 sudo ldconfig
 
 cd ..
 mkdir cuda-testing
-cp -a /usr/local/cuda-10.2/samples  cuda-testing/
+cp -a /usr/local/cuda-10.0/samples  cuda-testing/
 cd cuda-testing/samples
 make -k -j4
 
--- install-ubuntu1804-7-autoware1.13.0.sh
+++ install-ubuntu1804-7-autoware1.13.0.sh
@@ -12,3 +12,12 @@ cd utils
 wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
 mkdir eigen && tar --strip-components=1 -xzvf 3.3.7.tar.gz -C eigen
 cd eigen && mkdir build && cd build && cmake .. && make && make install
+
+cd
+mkdir -p autoware.ai/src
+cd autoware.ai
+wget -O autoware.ai.repos "https://gitlab.com/autowarefoundation/autoware.ai/autoware/raw/1.13.0/autoware.ai.repos?inline=false"
+vcs import src < autoware.ai.repos
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+AUTOWARE_COMPILE_WITH_CUDA=1 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
