97bf417ee64ec078a0d231c41b7f05fed6ea2159
Kelvin Kang <kelvinkang@cmu.edu>
2020-05-16 20:14:16 +0800

    fix cuda10.2 installation

---
+++ install-ubuntu1804-2-basic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "#######################First time update########################\n"
+sudo apt update
+sudo apt upgrade
+mkdir utils
+cd utils
+
+sudo apt install git htop clang-format curl wget software-properties-common apt-transport-https -y
+
+echo "#######################Downloading chrome#######################\n"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+echo "#######################Downloading typora#######################\n"
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt-get install typora -y
+
+echo "#######################Downloading psensor#######################\n"
+sudo apt install lm-sensors hddtemp -y
+sudo sensors-detect
+sensors
+sudo apt install psensor -y
+
--- install-ubuntu1804-3-ros.sh
+++ install-ubuntu1804-3-ros.sh
@@ -5,12 +5,12 @@ sudo apt update
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install -y ros-melodic-desktop-full
 sudo echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
-sudo apt install ros-${ROS_DISTRO}-rosserial ros-${ROS_DISTRO}-rosserial-server
+sudo apt install -y ros-${ROS_DISTRO}-rosserial ros-${ROS_DISTRO}-rosserial-server
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 catkin_make
