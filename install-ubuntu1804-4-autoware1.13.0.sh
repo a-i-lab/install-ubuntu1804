b9415d55a4e9658fbf5bd8d8abe8929bfd73cf99
Kelvin Kang <kelvinkang@cmu.edu>
2020-06-22 13:07:16 +0800

    update autoware inst

--- install-ubuntu1804-4-autoware1.13.0.sh
+++ install-ubuntu1804-4-autoware1.13.0.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# DIR=$(pwd)
+DIR=$(pwd)
 
-# echo "#######################install autoware########################\n"
-# sudo apt update
-# sudo apt upgrade -y
-# sudo apt install -y python-catkin-pkg python-rosdep ros-$ROS_DISTRO-catkin
-# sudo apt install -y python3-pip python3-colcon-common-extensions python3-setuptools python3-vcstool
-# pip3 install -U setuptools
-# python -m pip install tornado
-# pip install bson
-# cd utils
-# wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
-# mkdir eigen && tar --strip-components=1 -xzvf 3.3.7.tar.gz -C eigen
-# cd eigen && mkdir build && cd build && cmake .. && make && sudo make install
+echo "#######################install autoware########################\n"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python-catkin-pkg python-rosdep ros-$ROS_DISTRO-catkin
+sudo apt install -y python3-pip python3-colcon-common-extensions python3-setuptools python3-vcstool
+pip3 install -U setuptools
+python -m pip install tornado
+pip install bson
+mkdir utils
+cd utils
+wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
+mkdir eigen && tar --strip-components=1 -xzvf 3.3.7.tar.gz -C eigen
+cd eigen && mkdir build && cd build && cmake .. && make && sudo make install
 
 cd
 mkdir -p autoware.ai
